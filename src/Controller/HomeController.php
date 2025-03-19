f562f67fbcf98988d9a6f1781628f9b774dbc94c
vkrpk <victor.krupka@orange.fr>
2025-03-19 12:04:14 +0100

    Implémentation de l'algorithme génétique sur le cas du Voyageur itinérant (étape encodage des paramètres, calcul du score d’adaptation, survival of the fittest et Mutation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -103,33 +103,174 @@ class HomeController extends AbstractController
         return $memoire[$montant] == PHP_INT_MAX ? -1 : $memoire[$montant];
     }
 
+    public function shuffle_assoc($list) {
+        if (!is_array($list)) return $list;
+
+        $keys = array_keys($list);
+        shuffle($keys);
+        $random = ['PARIS' => ['x' => 48.8566, 'y' => 2.3522, 'nom' => 'PARIS']];
+        foreach ($keys as $key) {
+            $random[$key] = $list[$key];
+        }
+        return $random;
+    }
+
+    private function deg2rad (int $x){
+        return pi() * $x/180;
+    }
+
+    private function get_distance_m (int $lat1, int $lng1, int $lat2, int $lng2) {
+        $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+        $rlo1 = deg2rad($lng1);    // CONVERSION
+        $rla1 = deg2rad($lat1);
+        $rlo2 = deg2rad($lng2);
+        $rla2 = deg2rad($lat2);
+        $dlo = ($rlo2 - $rlo1) / 2;
+        $dla = ($rla2 - $rla1) / 2;
+        $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo));
+        $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        return (int) round(($earth_radius * $d) / 1000);
+    }
+
+    public function createChildren (array $villes, int $numberOfChildren): array
+    {
+        $chromosomes = [];
+
+        for ($i = 0; $i < $numberOfChildren; $i++) {
+            $villesCopy = $villes;
+
+            $distance = 0;
+
+            shuffle($villesCopy);
+
+            if(!in_array("PARIS", $this->extractCities($villes), true)) {
+                array_unshift($villesCopy, ['x' => 48.8566, 'y' => 2.3522, 'ville' => 'PARIS']);
+                $villesCopy[] = ['x' => 48.8566, 'y' => 2.3522, 'ville' => 'PARIS'];
+            }
+
+            $listLength = count($villesCopy);
+
+            for ($j = 0; $j < $listLength - 1; $j++) {
+                $distance += $this->get_distance_m($villesCopy[$j]['x'], $villesCopy[$j]['y'], $villesCopy[$j+1]['x'], $villesCopy[$j+1]['y']);
+            }
+
+            $chromosomes = $this->deleteDuplicates($villesCopy, $chromosomes, $distance);
+        }
+
+        usort($chromosomes, function($a, $b) {
+            return $a['distance'] <=> $b['distance'];
+        });
+
+        return $chromosomes;
+    }
+
+    public function extractCities (array $chromosome) {
+        return array_map(function($value) {
+            return $value['ville'];
+        }, $chromosome);
+    }
+
+    public function breedingAndCrossbreeding (array $chromosomes, int $crossoverPoint)
+    {
+//        $crossoverPoint = count($chromosomes[1]['parcours']) / 2;
+        $firstPart = array_slice($chromosomes[0]['parcours'], 0, $crossoverPoint);
+        $secondPart = array_slice($chromosomes[1]['parcours'], $crossoverPoint,  count($chromosomes[1]['parcours']) - $crossoverPoint);
+
+        $modifiedChromosome = array_merge($firstPart, $secondPart);
+
+        $cities = $this->extractCities($modifiedChromosome);
+
+//        dd( count(array_unique($this->extractCities($chromosomes[0]['parcours']))), count($this->extractCities($chromosomes[0]['parcours'])) - 1);
+
+        $i = 1;
+
+        while (count($cities) - 1 !== count(array_unique($cities))) {
+            $i++;
+
+            $modifiedChromosome = array_merge($firstPart, array_slice($chromosomes[$i]['parcours'], $crossoverPoint,  count($chromosomes[$i]['parcours']) - $crossoverPoint));
+        }
+
+        $nouvellePopulationDeChromosomes = $this->keepsTheBest($this->createChildren($modifiedChromosome, 100), 25);
+
+        dd($nouvellePopulationDeChromosomes);
+    }
+
+    public function algo_genetique ()
+    {
+        $villes = [
+            ['x' => 48.5734, 'y' => 7.7521, 'ville' => 'STRASBOURG'],
+            ['x' => 48.1173, 'y' => -1.6778, 'ville' => 'RENNES'],
+            ['x' => 43.7102, 'y' => 7.2620, 'ville' => 'NICE'],
+            ['x' => 47.4784, 'y' => -0.5632, 'ville' => 'ANGERS'],
+            ['x' => 47.2184, 'y' => -1.5536, 'ville' => 'NANTES'],
+            ['x' => 43.7383, 'y' => 7.4244, 'ville' => 'MONACO']
+        ];
+
+        $chromosomes = $this->keepsTheBest($this->createChildren($villes, 100), 25);
+
+        $this->breedingAndCrossbreeding($chromosomes, 1);
+
+        dump($chromosomes);
+    }
+
+    public function deleteDuplicates(array $villes, array $chromosomes, int $distance): array
+    {
+        $parcoursKey = '';
+        foreach ($villes as $ville) {
+            $parcoursKey .= $ville['ville'] . '|';
+        }
+
+        $existe = false;
+        foreach ($chromosomes as $chromosome) {
+            $existingKey = '';
+            foreach ($chromosome['parcours'] as $ville) {
+                $existingKey .= $ville['ville'] . '|';
+            }
+
+            if ($existingKey === $parcoursKey) {
+                $existe = true;
+                break;
+            }
+        }
+
+        if (!$existe) {
+            $chromosomes[] = ['parcours' => $villes, 'distance' => $distance];
+        }
+
+        return $chromosomes;
+    }
+
+
     #[Route('/', name: 'home')]
     public function index(ChartBuilderInterface $chartBuilder) {
-        $pieces = [2, 5, 10, 50, 100];
 
-//        dd($this->rendu_monnaie_naive($pieces, 150));
+        $this->algo_genetique();
 
-        $resultat20 = $this->rendu_monnaie_dynamique($pieces, 20);
-        $resultat70 = $this->rendu_monnaie_dynamique($pieces, 70);
-
-        return $this->render('base.html.twig', compact('resultat20', 'resultat70'));
-
-
-        $this->rendreLeMoinsDeMonnaiePossible(1462, 1500);
-
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-
-        $i = 0;
-
-
-        $tableOfOneHundredRandomNumbers = array_map(function ($value) {
-            static $i = 0;
-            $i++;
-            return $i;
-        }, array_fill(0, 10, null));
-
-
-        shuffle($tableOfOneHundredRandomNumbers);
+//        $pieces = [2, 5, 10, 50, 100];
+//
+////        dd($this->rendu_monnaie_naive($pieces, 150));
+//
+//        $resultat20 = $this->rendu_monnaie_dynamique($pieces, 20);
+//        $resultat70 = $this->rendu_monnaie_dynamique($pieces, 70);
+//
+//        return $this->render('base.html.twig', compact('resultat20', 'resultat70'));
+//
+//
+//        $this->rendreLeMoinsDeMonnaiePossible(1462, 1500);
+//
+//        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+//
+//        $i = 0;
+//
+//
+//        $tableOfOneHundredRandomNumbers = array_map(function ($value) {
+//            static $i = 0;
+//            $i++;
+//            return $i;
+//        }, array_fill(0, 10, null));
+//
+//
+//        shuffle($tableOfOneHundredRandomNumbers);
 //        dd($tableOfOneHundredRandomNumbers);
 
 //        $searchValue = 5;
@@ -174,5 +315,10 @@ class HomeController extends AbstractController
 //            'startNormalSearch' => $time_elapsed_secs,
 //            'chart' => $chart,
         ]);
+    }
+
+    private function keepsTheBest(array $list, int $int): array
+    {
+        return array_slice($list, 0, $int);
     }
 }
